2a650c591ac5d4aceb878b62fca6d0afd984b1e4
Zeynepge <sgunal571@gmail.com>
2023-04-10 09:28:11 +0200

    US_12_Zeynep feature tamamlanmistir.

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -49,7 +49,7 @@ public class Parent {
 
     public void scrollToElement(WebElement element){
         JavascriptExecutor js=(JavascriptExecutor) GWD.getDriver();
-        js.executeScript("arguments[0].scrollIntoView();",element);
+        js.executeScript("arguments[0].scrollIntoView(false);",element);
 
     }
 }
