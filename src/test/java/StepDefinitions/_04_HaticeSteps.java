b9dc12f421a3a1e1a09518830eae49c568d52fb1
31635 <haticedere4844@gmail.com>
2023-04-11 13:40:57 +0200

    Gonderildi

---
+++ src/test/java/StepDefinitions/_04_HaticeSteps.java
@@ -0,0 +1,30 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class _04_HaticeSteps {
+
+    DialogContent dc = new DialogContent();
+
+    @And("Enter employee Name")
+    public void enterEmployeeName(DataTable dt) {
+        List<List<String>> items = dt.asLists(String.class);
+
+        for (int i = 0; i < items.size(); i++) {
+            WebElement element = dc.getWebElement(items.get(i).get(0));
+            dc.sendKeysFunction(element, items.get(i).get(1));
+
+
+        }
+    }
+
+
+
+
+
+}
