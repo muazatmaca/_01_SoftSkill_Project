8226f2da0cd0b73810bfece09944a6914034eae8
aynur <ay.oktay79@gmail.com>
2023-04-14 20:09:31 +0200

    Aynur

--- src/test/java/StepDefinitions/_03_Aynur.java
+++ src/test/java/StepDefinitions/_03_Aynur.java
@@ -1,29 +1,32 @@
 package StepDefinitions;
 
 import Pages.DialogContent;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class _03_Aynur {
 
     DialogContent dc=new DialogContent();
+    @And("Click on the element in Dialog Content")
+    public void clickOnTheElementInDialogContent(DataTable dt) {
+        List<String> strButtons=dt.asList(String.class);
 
-    @And("Click on add button")
-    public void clickOnAddButton(){
-        dc.clickFunction(dc.addButton);
-
+        for (String strButton: strButtons) {
+            WebElement element=dc.getWebElement(strButton);
+            dc.clickFunction(element);
+        }
     }
-
-    @And("Click on save button")
-    public void clickOnSaveButton(){
-        dc.clickFunction(dc.saveButton);
-    }
-
 
     @Then("Required text is displayed")
     public void requiredTextIsDisplayed() {
 
         dc.verifyContainsTextFunction(dc.requiredText,"Required");
     }
+
+
 }
 
--- src/test/java/StepDefinitions/_08_Aynur.java
+++ src/test/java/StepDefinitions/_08_Aynur.java
@@ -1,17 +1,23 @@
 package StepDefinitions;
 
 import Pages.DialogContent;
+import Pages.LeftNav;
 import io.cucumber.java.en.And;
 
 import org.testng.Assert;
 
 public class _08_Aynur {
+
+    LeftNav ln=new LeftNav();
     DialogContent dc=new DialogContent();
+    @And("Click on the element in LeftNav")
+    public void clickOnTheElementInLeftNav() {
+
+    }
     @And("Add button is displayed")
     public void addButtonIsDisplayed()
     {
         dc.verifyContainsTextFunction(dc.addButton,"Add");
     }
-
 
 }
